006d692bdaf1de9a3c6b4114a6de2bdd8061d1d9
skdonnet <donovan.ruebusch@student.swic.edu>
2021-04-20 23:20:42 -0500

    min font/max font scripts

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const p = document.querySelector('p');
 const root = document.querySelector(':root');
 const txt = document.getElementById('txt');
+const form = document.querySelectorAll('form');
 
 document.addEventListener('keyup', event => {
   p.innerText = txt.value;
@@ -16,4 +17,18 @@ document
   .querySelector('input[type="color"]')
   .addEventListener('input', event => {
     root.style.setProperty('--color', `${event.target.value}`);
+  });
+
+document
+  .querySelector('#min-font-size-form')
+  .addEventListener('keyup', event => {
+    event.preventDefault();
+    document.querySelector('#font-size').min = event.target.value;
+  });
+
+document
+  .querySelector('#max-font-size-form')
+  .addEventListener('keyup', event => {
+    event.preventDefault();
+    document.querySelector('#font-size').max = event.target.value;
   });
